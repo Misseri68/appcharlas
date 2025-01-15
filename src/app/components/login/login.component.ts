d2a6c869a87ef124c4b2d8a0936c6a12158022f9
AlexRR03 <alexciempo03@gmail.com>
2025-01-15 10:37:08 +0100

    login-standalone

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LoginService } from '../../services/login.service';
-
+import { CommonModule } from '@angular/common';  // Necesario para directivas comunes
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [CommonModule]  // Importa CommonModule aquí
 })
 export class LoginComponent {
     @ViewChild("cajaemail") cajaEmail!: ElementRef;
