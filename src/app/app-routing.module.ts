dfe603e4da7606cb2f08aafadb3b6761b6b3cf40
Misseri68 <privatedari@gmail.com>
2025-01-16 09:13:59 +0100

    correccion router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/loginpruebaari/login.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {
