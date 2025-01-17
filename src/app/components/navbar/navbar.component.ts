e4c4bd7fad85d306a8f3608de6023fadabf9c8e5
Misseri68 <70648183+Misseri68@users.noreply.github.com>
2025-01-17 13:54:47 +0100

    navbar ya muestra imagen de perfil

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,13 +12,29 @@ import { LoginService } from '../../services/login.service';
 })
 export class NavbarComponent {
 
-  constructor(private _loginService: LoginService, private _router: Router) { }
+  imagen :string = 'assets/images/test.jpg';
+
+  constructor(private _loginService: LoginService, private _userServ: UserService, private _router: Router) { 
+    this.getImagenPerfil();
+  }
 
   showPopup: boolean = false;
 
   togglePopup(): void {
     this.showPopup = !this.showPopup; // Alterna la visibilidad del pop-up
     console.log('Popup visible:', this.showPopup);
+  }
+
+  getImagenPerfil(){
+    console.log("Hola")
+    this._userServ.getPerfil().then(perfil => {
+      if(perfil?.imagen){
+        this.imagen = perfil.imagen;
+        console.log(this.imagen)
+      }else{
+        this.imagen = ''
+      }
+    })
   }
 
   logout(): void {
