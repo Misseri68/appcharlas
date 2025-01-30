41284b4df010ba14d8b2817555c6f5f6655b37c4
Gabriel Fonseca <gabrielfonsecag@gmail.com>
2025-01-30 15:08:47 +0100

    pop up de edit profe en admin ready

--- src/app/components/control-panel/control-panel.component.ts
+++ src/app/components/control-panel/control-panel.component.ts
@@ -7,18 +7,34 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-control-panel',
   standalone: true,
-  imports: [CommonModule, FormsModule], 
+  imports: [CommonModule, FormsModule],
   templateUrl: './control-panel.component.html',
   styleUrl: './control-panel.component.scss',
   providers: [AdminService]
 })
 export class ControlPanelComponent implements OnInit {
   public profesores: Profesor[] = [];
-  public profesorSeleccionado: Profesor | null = null;
+  public profesorSeleccionado: Profesor = {
+
+    idUsuario: 0,
+    usuario: '',
+    estadoUsuario: false,
+    imagen: '',
+    email: '',
+    idRole: 0,
+    role: '',
+    idCurso: 0,
+    curso: '',
+    fechaInicioCurso: '',
+    fechaFinCurso: '',
+    idCursosUsuarios: 0
+  };
+
+
   public mostrarModal: boolean = false;
 
 
-  constructor(private _adminService: AdminService) {}
+  constructor(private _adminService: AdminService) { }
 
   ngOnInit(): void {
     this.loadProfesores();
@@ -33,10 +49,12 @@ export class ControlPanelComponent implements OnInit {
 
   // Método para seleccionar un profesor y abrir el modal
   seleccionarProfesor(profesor: Profesor) {
-    this.profesorSeleccionado = { ...profesor }; // Clonamos el objeto para evitar cambios directos en la lista
+    this.profesorSeleccionado = { ...profesor };
+
     this.mostrarModal = true;
     console.log("✅ Seleccionar profesor:", this.profesorSeleccionado);
   }
+
 
   // Método para cerrar el modal
   cerrarModal() {
@@ -47,7 +65,7 @@ export class ControlPanelComponent implements OnInit {
   actualizarProfesor() {
     if (this.profesorSeleccionado) {
 
-     
+
     }
   }
 
